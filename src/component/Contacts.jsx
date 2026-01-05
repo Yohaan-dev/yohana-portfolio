009d218887f3cbf54d23a1777544aa9dcba94cd9
Yohaan <yohanakipruto@gmail.com>
2026-01-05 04:44:48 +0300

    Initial Commit

--- src/component/Contacts.jsx
+++ src/component/Contacts.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import {useState} from 'react'
 import { FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import SuccessToast from "./SuccessToast.jsx";
 
 const Contacts = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [message, setMessage] = useState('')
+    const [success, setSuccess] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //console.log({name, email, subject, message, success})
+        setSuccess(true);
+
+        //Clear Form Fields
+        setName(''),
+        setEmail(''),
+        setSubject(''),
+        setMessage('')
+    };
+        
   return (
       <section className='max-w-7xl mx-auto p-4 mt-20 py-20' id='contact'>
           <div className='flex flex-col items-center py-4 border-b border-zinc-500/20 w-full'>
@@ -28,23 +47,48 @@ const Contacts = () => {
                   </div>
               </div>
               <div className='flex flex-col space-y-4'>
-                  <div className='flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2'>
-                      <input className='w-full rounded-md px-4 py-2 bg-zinc-600 text-zinc-300 placeholder-gray-400 focus:outline-none focus-ring-none' type="text" placeholder='Your Name' required />
-                      <input className='w-full rounded-md px-4 py-2 bg-zinc-600 text-zinc-300 placeholder-gray-400 focus:outline-none focus-ring-none' type="email" placeholder='Your Email' required />
+                  <form onSubmit={handleSubmit}>
+                      <div className='flex flex-col md:flex-row items-center space-y-2 my-3 md:space-y-0 md:space-x-2'>
+                          <div className='w-full'>
+                              <input className='rounded-md px-4 py-2 bg-zinc-600 text-zinc-300 placeholder-gray-400 focus:outline-none focus-ring-none'
+                                  type="text"
+                                  placeholder='Your Name'
+                                  value={name}
+                                  onChange={(e) => setName(e.target.value)}
+                                  required />
+                          </div>
+                          <div className='w-full'>
+                              <input className='rounded-md px-4 py-2 bg-zinc-600 text-zinc-300 placeholder-gray-400 focus:outline-none focus-ring-none'
+                                  type="email"
+                                  placeholder='Your Email'
+                                  value={email}
+                                  onChange={(e) => setEmail(e.target.value)}
+                                  required />
+                      </div>
                   </div>
                   <div>
-                      <input className='w-full rounded-md px-4 py-2 bg-zinc-600 text-zinc-300 placeholder-gray-400 focus:outline-none focus-ring-none' type='text' placeholder='Subject' required />
+                          <input className='w-full rounded-md px-4 py-2 my-3 bg-zinc-600 text-zinc-300 placeholder-gray-400 focus:outline-none focus-ring-none'
+                              type='text'
+                              placeholder='Subject'
+                              value={subject}
+                              onChange={(e) => setSubject(e.target.value)}
+                              required />
                   </div>
-                  <div>
+                  <div className='my-3'>
                         <textarea
                             rows="4"
                             placeholder="Your Message"
                             className="w-full rounded-md bg-zinc-600 px-4 py-2
                                text-zinc-300 placeholder-gray-400
                              focus:outline-none  focus:ring-none"
+                              value={message}
+                              onChange={(e) => setMessage(e.target.value)}
                         ></textarea>
                   </div>
-                  <button className='rounded-full text-white font-serif bg-amber-600 px-3 py-1 hover:bg-amber-500 flex items-center gap-2 w-fit'>Send Message</button>
+                      <button className='rounded-full text-white font-serif bg-amber-600 px-3 py-1 hover:bg-amber-500 flex items-center gap-2 w-fit'>Send Message</button>
+                  </form>
+                   <SuccessToast message="Form submitted successfully!" show={success} onClose={() => setSuccess(false)}
+      />
               </div>
           </div>
     </section>
